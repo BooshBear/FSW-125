aded2a447d67df46bf8fff7491b0311930ffb794
BooshBear <dylan.mailloux@bryanuniversity.edu>
2022-01-30 21:16:05 -0800

    Week 1 Done

--- Week1/Assignments/math/app.js
+++ Week1/Assignments/math/app.js
@@ -1,8 +1,8 @@
 let req = require("./math")
 
 // req(number 1, number 2) --->
-let num = new req()
-console.log("Addition: " + num.add(1, 1))
-console.log("Subtraction: " + num.sub(6, 3))
-console.log("Multiplication: " + num.mul(4, 2))
-console.log("Division: " + num.div(5, 2))
+let num = new req(1, 2)
+console.log("Addition: " + num.add());
+console.log("Subtraction: " + num.sub());
+console.log("Multiplication: " + num.mul());
+console.log("Division: " + num.div());
